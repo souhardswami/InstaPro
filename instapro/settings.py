a3c5ce68ff6ddb780bc91ba13cc00e2c6930f276
souhardswami <48220329+souhardswami@users.noreply.github.com>
2020-05-08 23:17:59 +0530

    Add files via upload

--- instapro/settings.py
+++ instapro/settings.py
@@ -48,6 +48,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
     'django.middleware.common.CommonMiddleware',
 
 
@@ -133,6 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 
