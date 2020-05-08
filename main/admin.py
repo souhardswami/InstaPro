eed9cd5ee8057567f692693aef1b03d4a70b5457
souhardswami <48220329+souhardswami@users.noreply.github.com>
2020-05-08 22:55:12 +0530

    Add files via upload

---
+++ main/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Photos,Users,Comment,Like,Connection
+
+# Register your models here.
+
+
+admin.site.register(Photos)
+admin.site.register(Users)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Connection)
